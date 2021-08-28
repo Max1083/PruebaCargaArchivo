c7f2078c55eb9f0ac98669245fb1aedfc6a2d07d
Max1083 <maximiliano.sanchezv@outlook.com>
2021-08-28 12:56:06 -0400

    Configuracion

--- PruebaCarga/PruebaCarga/settings.py
+++ PruebaCarga/PruebaCarga/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import PruebaCarga.db as db
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,12 +75,7 @@ WSGI_APPLICATION = 'PruebaCarga.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = db.MYSQL
 
 
 # Password validation
